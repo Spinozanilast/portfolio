bb668901c396853090507baf7a7c56ac886513d2
Vadim Budchanin <107877150+Spinozanilast@users.noreply.github.com>
2024-07-02 19:14:58 +0300

    add dynamic behaviour to page

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+const CATEGORY_BUTTONS_CLASSNAME = "category-button";
+const ARTICLES_CLASSNAME = "article-section";
+const ANIMATED_BACKGROUND_CLASSNAME = "animated-section-background";
+
+const setCategoryPressAnimations = () => {
+    const categoriesButtonsList = document.getElementsByClassName(CATEGORY_BUTTONS_CLASSNAME);
+    const articlesList = document.getElementsByClassName(ARTICLES_CLASSNAME);
+
+    for (let i = 0; i < categoriesButtonsList.length && articlesList.length; i++) {
+        categoriesButtonsList[i].addEventListener("click", function () {
+            articlesList[i].classList.add(ANIMATED_BACKGROUND_CLASSNAME);
+            setTimeout(() => {
+                articlesList[i].classList.remove(ANIMATED_BACKGROUND_CLASSNAME);
+            }, 1000);
+        });
+    }
+}
+
+setCategoryPressAnimations();
