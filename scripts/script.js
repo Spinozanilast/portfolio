4f432584427bfe63411470c4889bad8b89032e80
Vadim Budchanin <107877150+Spinozanilast@users.noreply.github.com>
2024-07-07 23:15:59 +0300

    add theme toggling

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,20 @@
 const CATEGORY_BUTTONS_CLASSNAME = "category-button";
 const ARTICLES_CLASSNAME = "article-section";
 const ANIMATED_BACKGROUND_CLASSNAME = "animated-section-background";
+const THEMES = {
+    DARK: {
+        name: "dark",
+        iconPath: "./assets/NightVector.svg"
+    },
+    LIGHT: {
+        name: "light",
+        iconPath: "./assets/LightVector.svg"
+    }
+}
 
-function setCategoryPressAnimations() {
+const DEFAULT_THEME = THEMES.DARK;
+
+(function setCategoryPressAnimations() {
     const categoriesButtonsList = document.getElementsByClassName(CATEGORY_BUTTONS_CLASSNAME);
     const articlesList = document.getElementsByClassName(ARTICLES_CLASSNAME);
 
@@ -14,6 +26,35 @@ function setCategoryPressAnimations() {
             }, 1000);
         });
     }
+})();
+
+let currentTheme = DEFAULT_THEME;
+
+function changeTheme(themeButton, themeImageId) {
+    const prevTheme = currentTheme;
+    themeButton.classList.remove(currentTheme.name);
+    document.getElementById(themeImageId).classList.remove(currentTheme.name);
+
+    currentTheme = currentTheme === THEMES.DARK
+        ? THEMES.LIGHT
+        : THEMES.DARK;
+    document.querySelector("html").setAttribute("data-theme", currentTheme.name);
+
+    themeButton.classList.add(currentTheme.name);
+    const imgElement = document.getElementById(themeImageId);
+    imgElement.classList.add(currentTheme.name);
+    imgElement.src = currentTheme.iconPath;
+
+    changeIconsTheme(prevTheme.name);
 }
 
-setCategoryPressAnimations();
+function changeIconsTheme(prevTheme) {
+    document.querySelectorAll(`.reference>img`).forEach((refIcon) => {
+        const refClassList = refIcon.classList;
+        refClassList.remove(refClassList.item(0));
+        refClassList.add(currentTheme.name);
+    })
+    document.querySelectorAll('img.hobby-icon').forEach((refIcon) => {
+        refIcon.src = refIcon.src.replace(currentTheme.name, prevTheme);
+    })
+}
